954e1fe03208e8f49f75e67324f28e2a5ae7ae81
Chris Song <ceo@elgamez.com>
2015-01-16 00:10:16 +0900

    Day14 Added

---
+++ Day14/Assets/Blade_girl/scripts/Common/loadLevelbutton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevelbutton : MonoBehaviour {
+
+	public Texture2D normalTex;
+	public Texture2D hoverTex;
+
+	public int loadLevel;
+
+
+
+
+
+
+	private void OnMouseEnter ()
+	{
+
+		guiTexture.texture = hoverTex;
+	}
+
+	private void OnMouseExit ()
+	{
+
+		guiTexture.texture = normalTex;
+	}
+
+
+	private void OnMouseDown()
+	{
+		Application.LoadLevel(loadLevel);
+
+	}
+
+
+
+
+
+
+
+}
