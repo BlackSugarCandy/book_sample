82a7990e75eb9c6667b951a1340ca9c30ab7716a
Chris Song <ceo@elgamez.com>
2015-01-16 03:04:51 +0900

    Navigation 길찾기  AI 추가함

---
+++ Day3-1/Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+	
+	Transform player;
+	NavMeshAgent nav;
+
+	void Awake () {
+		
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+
+	}
+	
+	void Update () {
+		
+		nav.SetDestination (player.position);
+	}
+}
